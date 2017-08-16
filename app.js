0814afe2bc35bc329daa32f57518339f0280c475
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-08-16 08:34:18 -0400

    Add form

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -21,6 +22,13 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Body-parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 // Home route
 app.get('/', function(req, res) {
   Article.find({}, function(err, articles) {
@@ -39,6 +47,22 @@ app.get('/', function(req, res) {
 app.get('/articles/add', function(req, res) {
   res.render('add_article', {
     title: 'Articles'
+  });
+});
+
+// Add submit POST route
+app.post('/articles/add', function(req, res) {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+  article.save(function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
