4bfa306de75e29945647474b0c3a9b88eee46340
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-08-16 10:59:16 -0400

    Add update and delete functionality

--- app.js
+++ app.js
@@ -78,5 +78,46 @@ app.post('/articles/add', function(req, res) {
   });
 });
 
+// Load edit form
+app.get('/article/edit/:id', function(req, res) {
+  Article.findById(req.params.id, function(err, article) {
+    res.render('edit_article', {
+      title: 'Edit Article',
+      article: article
+    });
+  });
+});
+
+// Update submit POST route
+app.post('/articles/edit/:id', function(req, res) {
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id: req.params.id};
+
+  Article.update(query, article, function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+// Delete article
+app.delete('/article/:id', function(req, res) {
+  let query = {_id: req.params.id};
+
+  Article.remove(query, function(err) {
+    if (err) {
+      console.log(err);
+    }
+    res.send('Success');
+  })
+});
+
 // Start server
 app.listen(3000, () => console.log('Server started on port 3000'));
